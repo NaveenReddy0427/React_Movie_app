916b09c0e66af123fc565e1ed17a7e73ad45a216
NaveenReddy0427 <naveendeva909@gmail.com>
2024-03-27 14:30:06 +0530

    do create sperate file for  Navbar using CSS module

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,73 +1,30 @@
 import React from "react";
-import './styles.css';
-import styled from "styled-components"
-
-const Nav =  styled.div`
-    height: 70px;
-	background: linear-gradient(170deg, #1bc059, #0d47a1);
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	position: relative;
-  
-`;
-
-const Title = styled.div`
-    font-size: 30px;
-    color: #fff;
-    font-weight: 600;
-    font-family: 'Times New Roman', Times, serif;
-    text-transform: uppercase;
-    margin-left: 20px ;
-    &:hover{color: yellow}
-`;
-
-
-const CartImg = styled.img`
-    height: 48px;
-    margin-right: 20px;
-`;
-
-const CartIconContainer = styled.div`
-    position: relative;
-    cursor: pointer;
-`;
-
-const CartCount = styled.div`
-    background-color: ${(props)=> props.color};
-    border-radius: 50%;
-    padding: 4px 8px;
-    position: absolute;
-    right: 10px;
-    top: -5px;
-    font-size: 12px;
-    visiblility: ${(props)=> props.show? "visible": "hidden"};
-`;
-
-
+import styles from './Navbar.module.css'
 
 class Navbar extends React.Component {
 
-  
     render() {   
        
       return (
       <>
-      <Nav>
+      <div className={styles.nav}>
         
-      <Title>MOVIE APP</Title>
+      <div className={styles.title}>MOVIE APP</div>
         
-        <CartIconContainer>
-            <CartImg alt="Cart-Icon" 
+        <div className={styles.cartIconContainer}>
+            <img className={styles.cartIcon} 
+                 alt="Cart-Icon" 
                  src = "https://cdn-icons-png.flaticon.com/128/891/891462.png"    
             />
-            <CartCount color="orange" show={false}>5</CartCount>
-        </CartIconContainer>
-      </Nav>
+            <span className ={styles.cartCount}>5</span>
+        </div>
+      </div>
 
       </>
       )
     }
   }  
-
-export default Navbar;
+ 
+  
+  export default Navbar;
+  
